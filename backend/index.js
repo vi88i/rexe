3b1e82e18448472989a181f111fa739c0dff78d1
Vighnesh Nayak S <vighneshsnayak@gmail.com>
2021-10-01 18:43:20 +0530

    added basic http server template

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/* load the .env to access constants */
+require('dotenv').config();
+
+/* require all modules here */
+const express = require('express');
+
+/* create express application */
+const app = express();
+
+/* 404 responses */
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
+/* global error handler */
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    next(err);
+  } else if (err.msg && err.status) {
+    res.status(err.status).send(err.msg);
+  } else {
+    res.status(500).send('Something went wrong!');
+  }
+});
+
+/* start listening for connections */
+app.listen(process.env.PORT, process.env.HOST, process.env.BACKLOG, (err) => {
+  if (err) {
+    console.log(err);
+    process.exit(0);
+  } else {
+    console.log(`Server listening on port ${process.env.PORT}`);
+  }
+});
