b5b783aa77cf02fe0cae4a81934d0bed8e382692
Vighnesh Nayak S <vighneshsnayak@gmail.com>
2021-10-10 22:16:47 +0530

    added redis support for sign out and duplication

--- rexe-backend/index.js
+++ rexe-backend/index.js
@@ -13,6 +13,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
 const AWS = require('aws-sdk');
+const redis = require('redis');
 const express = require('express');
 const db = require('./db/index');
 
@@ -20,18 +21,40 @@ const db = require('./db/index');
 AWS.config.loadFromPath(path.join('..', 'aws_config.json'));
 const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 
+/* setup connection to token blacklist database and user submission request database */
+const redisAuthClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  db: process.env.REDIS_BLACKLIST_DB
+});
+
+const redisSubClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  db: process.env.REDIS_SUBMISSION_DB  
+});
+
+redisAuthClient.on('error', (err) => {
+  console.log(err);
+  process.exit(0);
+});
+
+redisSubClient.on('error', (err) => {
+  console.log(err);
+  process.exit(0);
+});
+
 /* create express application */
 const app = express();
 
-/* add JSON body parser */
-app.use(express.json());
-/* add cookie parser middleware */
+/* add JSON body parser and cookie parser */
+app.use(express.json({ limit: process.env.JSON_LIMIT }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 /* basic logging for requests */
 app.use((req, _, next) => {
   if (environment === 'development') {
-    console.log(new Date().toISOString() + ' ' + req.method + ' ' + req.url + ' ');
+    console.log(new Date().toISOString() + ' ' + req.method + ' ' + req.url);
   }
   next();
 });
@@ -69,7 +92,7 @@ app.post('/verify', async (req, res, next) => {
       conn = await db.getConnection();
       const { rows } = await db.query(conn, 'SELECT COUNT(*) AS count FROM users WHERE username = ?', [username]);
       if (rows[0].count > 0) {
-        throw { status: 409, msg: 'Username already exists!' };
+        throw { status: 400, msg: 'Username already exists!' };
       }
       const hash = crypto.createHash('sha256').update(password).digest('hex');
       await db.query(conn, 'INSERT INTO users VALUES(?, ?)', [username, hash]);
@@ -146,15 +169,24 @@ app.get('/isLoggedIn', (req, res) => {
 
 /* authorization middleware */
 const requireAuthorization = (req, res, next) => {
-  if (req.signedCookies) {
+  if (req.signedCookies && req.signedCookies.token) {
     let token = req.signedCookies.token;
-    jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
+    redisAuthClient.get(token, (err, reply) => {
       if (err) {
-        next({ status: 401, msg: 'Unauthorized' });
+        next(err);
+      } else if (reply) {
+        res.status(401).json({ msg: 'Unauthorized' });
       } else {
-        res.locals.username = decoded.username;
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+          if (err) {
+            next({ status: 401, msg: 'Unauthorized' });
+          } else {
+            res.locals.username = decoded.username;
+            res.locals.ttl = decoded.exp - Math.floor(Date.now() / 1000);
+          }
+          next();
+        }); 
       }
-      next();
     });    
   } else {
     next({ status: 401, msg: 'Unauthorized' });
@@ -162,70 +194,116 @@ const requireAuthorization = (req, res, next) => {
 };
 
 /* sign out the user */
-app.get('/sign-out', requireAuthorization, (req, res) => {
-  // TODO: blacklist token in redis till TTL
-  res.clearCookie('username');
-  res.clearCookie('token');
-  res.status(200).json({ msg: 'ok' });
+app.get('/sign-out', requireAuthorization, async (req, res) => {
+  try {
+    if (req.signedCookies && req.signedCookies.token) {
+      if (res.locals.ttl > 0) {
+        await new Promise((resolve, reject) => {
+          redisAuthClient.set(req.signedCookies.token, '1', 'EX', res.locals.ttl, (err) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve();
+            }
+          });
+        });
+      }
+      res.clearCookie('username');
+      res.clearCookie('token');
+      res.status(200).json({ msg: 'ok' });
+    } else {
+      res.status(401).json({ msg: 'Unauthorized' });
+    }
+  } catch(err) {
+    next(err);
+  }
 });
 
 /* process code submission */
-app.post('/run', requireAuthorization, (req, res, next) => {
-  /*
-    TODO:
-    1. Check in cache
-    2. Queue on SQS
-    3. Prevent duplicate request by same user on same file
-  */
-  const { code, filename, lang, time_limit, memory_limit } = req.body;
-  const username = res.locals.username;
-
-  if (filename && filename.length > 0 && code && code.length > 0) {
-    if (lang && ['cpp', 'py'].indexOf(lang) === -1) {
-      throw { status: 200, msg: 'Invalid language code' };
-    }
-
-    let mb = Math.floor(Number(memory_limit)); 
-    let sec = Math.floor(Number(time_limit));
-
-    if (Number.isNaN(mb) || Number.isNaN(sec) || mb < 1 || mb > 512 || sec < 1 || sec > 7) {
-      throw { status: 200, msg: 'Invalid memory or time limit' };
-    }
-
-    const packet = {
-      ...req.body,
-      memory_limit: mb,
-      time_limit: sec
-    };
-
-    const params = {
-      MessageAttributes: {
-        "author": {
-          DataType: "String",
-          StringValue: username
-        },
-        "filename": {
-          DataType: "String",
-          StringValue: filename          
-        }
-      },
-      MessageBody: JSON.stringify(packet),
-      MessageDeduplicationId: username + '-' + lang + '-' + filename, 
-      MessageGroupId: "Group1",    
-      QueueUrl: lang === 'cpp' ? process.env.SQS_CPP_URL : process.env.SQS_PY_URL   
-    };
-    
-    sqs.sendMessage(params, (err, data) => {
-      if (err) {
-        console.log("Error", err);
-        res.status(200).json({ msg: 'Failed to process your request' });
-      } else {
-        console.log("Success", data.MessageId);
-        res.status(200).json({ msg: 'Your code is being processed' });
+app.post('/run', requireAuthorization, async (req, res, next) => {
+  try {
+    const { code, filename, lang, time_limit, memory_limit } = req.body;
+    const username = res.locals.username;
+  
+    if (filename && filename.length > 0 && code && code.length > 0) {
+      if (lang && ['cpp', 'py'].indexOf(lang) === -1) {
+        throw { status: 400, msg: 'Invalid language code' };
       }
-    });    
-  } else {
-    res.status(200).json({ msg: 'Filename and code cannot be empty' });
+  
+      const submissionKey = username + '-' + filename;
+      const status = await new Promise((resolve, reject) => {
+        redisSubClient.get(submissionKey, (err, reply) => {
+          if (err) {
+            reject(err);
+          } else {
+            // reply is empty, if key is not found
+            resolve(reply);
+          }
+        });
+      });
+  
+      if (status) {
+        throw { status: 429, msg: 'Your previous submission for same problem is still being processed' };
+      }
+  
+      await new Promise((resolve, reject) => {
+        redisSubClient.set(submissionKey, '1', 'EX', process.env.SQS_SUBMISSON_RETENTION_PERIOD, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+  
+      let mb = Math.floor(Number(memory_limit)); 
+      let sec = Math.floor(Number(time_limit));
+  
+      if (Number.isNaN(mb) || Number.isNaN(sec) || mb < 1 || mb > 512 || sec < 1 || sec > 7) {
+        throw { status: 400, msg: 'Invalid memory or time limit' };
+      }
+  
+      const packet = {
+        ...req.body,
+        memory_limit: mb,
+        time_limit: sec
+      };
+  
+      const params = {
+        MessageAttributes: {
+          "author": {
+            DataType: "String",
+            StringValue: username
+          },
+          "filename": {
+            DataType: "String",
+            StringValue: filename          
+          },
+          "lang": {
+            DataType: "String",
+            StringValue: lang          
+          }         
+        },
+        MessageBody: JSON.stringify(packet),
+        MessageDeduplicationId: username + '-' + lang + '-' + filename, 
+        MessageGroupId: "Group1",    
+        QueueUrl: lang === 'cpp' ? process.env.SQS_CPP_URL : process.env.SQS_PY_URL   
+      };
+      
+      sqs.sendMessage(params, (err, data) => {
+        if (err) {
+          console.log("Error", err);
+          res.status(200).json({ msg: 'Failed to process your request' });
+        } else {
+          console.log("Success", data.MessageId);
+          res.status(200).json({ msg: 'Your code is being processed' });
+        }
+      });
+    } else {
+      res.status(400).json({ msg: 'Filename and code cannot be empty' });
+    }
+  } catch (err) {
+    next(err);
   }
 });
 
